24a371a4051d776b08eaaafaa7ece0453aa14485
Christian Berg <christian.berg@yetimove.com>
2023-01-02 21:26:26 +0100

    Add doxygen stuff

--- doc/conf.py
+++ doc/conf.py
@@ -169,4 +169,14 @@ texinfo_documents = [
 ]
 
 
-
+doxygen_root = "docs/doxygen" # this is just a convenience variable
+doxylink = {
+    "demo": (  # "demo" is the role name that you can later use in sphinx to reference this doxygen documentation (see below)
+        f"{doxygen_root}/demo/html/tagfile.xml", # the first parameter of this tuple is the tagfile
+        f"{doxygen_root}/demo/html", # the second parameter of this tuple is a relative path pointing from
+                                     # sphinx output directory to the doxygen output folder inside the output
+                                     # directory tree.
+                                     # Doxylink will use the tagfile to get the html file name of the symbol you want
+                                     # to link and then prefix it with this path to generate html links (<a>-tags).
+    ),
+}
